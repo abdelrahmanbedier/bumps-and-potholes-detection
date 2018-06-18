7c9f48ac2ed358d0a1c172fba3c24c5ef078a089
abdelrahmanbedier <34692503+abdelrahmanbedier@users.noreply.github.com>
2018-06-18 08:14:42 +0200

    Added Search Engine ,AutoComplete and OnLocationChange move

--- GoogleMapsBumps/app/src/main/java/com/example/bedier/googlemapsbumps/MapActivity.java
+++ GoogleMapsBumps/app/src/main/java/com/example/bedier/googlemapsbumps/MapActivity.java
@@ -6,6 +6,8 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 import android.os.Handler;
@@ -15,17 +17,26 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -38,7 +49,9 @@ import com.google.firebase.database.ValueEventListener;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
@@ -46,37 +59,7 @@ import java.util.UUID;
  * Created by User on 10/2/2017.
  */
 
-public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
-    DatabaseReference rootRef,demoRef;
-void detect_bump(){
-
-    if (mLocationPermissionsGranted) {
-        getDeviceLocation();
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        mMap.setMyLocationEnabled(true);
-        mMap.getUiSettings().setMyLocationButtonEnabled(false);
-
-    }
-}
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        Toast.makeText(this, "Map is Ready", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onMapReady: map is ready");
-        mMap = googleMap;
-        try {
-            findBT();
-            openBT();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback,GoogleApiClient.OnConnectionFailedListener,LocationListener {
 
 
     private static final String TAG = "MapActivity";
@@ -85,13 +68,24 @@ void detect_bump(){
     private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
     private static final float DEFAULT_ZOOM = 15f;
+    private static final LatLngBounds LAT_LNG_BOUNDS = new LatLngBounds(new LatLng(-40,168),new LatLng(71,136));
+
     public double longitude;
     public double latitude;
+
+    //widgets
+    private AutoCompleteTextView mSearchText;
+
+
     //vars
     private Boolean mLocationPermissionsGranted = false;
     private GoogleMap mMap;
     private FusedLocationProviderClient mFusedLocationProviderClient;
-//bluetooth var
+    private Location location;
+    private LocationSource.OnLocationChangedListener mListener;
+    private GoogleApiClient mGoogleApiClient;
+    private PlaceAutocompleteAdapter mPlaceAutocompleteAdapter;
+    //bluetooth var
 
 
     BluetoothAdapter mBluetoothAdapter;
@@ -106,16 +100,137 @@ void detect_bump(){
     volatile boolean stopWorker;
     static final int REQUEST_LOCATION = 1;
 
+    DatabaseReference rootRef,demoRef;
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Toast.makeText(this, "Map is Ready", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onMapReady: map is ready");
+        mMap = googleMap;
+        if (mLocationPermissionsGranted) {
+            getCurrentLocation();
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            mMap.setMyLocationEnabled(true);
+            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+            init();
+        }
+
+
+        try {
+            findBT();
+            openBT();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        mSearchText =(AutoCompleteTextView) findViewById(R.id.input_search);
         rootRef = FirebaseDatabase.getInstance().getReference();
         demoRef = rootRef.child("Coordinates");
         getLocationPermission();
     }
+    @Override
+    public void onLocationChanged(Location location) {
+            /* Push location updates to the registered listener..
+             * (this ensures that my-location layer will set the blue dot at the new/received location) */
+        if (mListener != null) {
+            mListener.onLocationChanged(location);
+        }
 
+            /* ..and Animate camera to center on that location !
+             * (the reason for we created this custom Location Source !) */
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(), location.getLongitude())));
+    }
+
+
+    void detect_bump(){
+
+        //if (mLocationPermissionsGranted) {
+        getDeviceLocation();
+        //  if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+        //        != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+        //      Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        // return;
+        //}
+        //mMap.setMyLocationEnabled(true);
+        //mMap.getUiSettings().setMyLocationButtonEnabled(false);
+
+//    }
+    }
+    private void init(){
+        Log.d(TAG, "init: initializing");
+
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .enableAutoManage(this, this)
+                .build();
+
+        mPlaceAutocompleteAdapter = new PlaceAutocompleteAdapter(this, mGoogleApiClient,
+                LAT_LNG_BOUNDS, null);
+
+        mSearchText.setAdapter(mPlaceAutocompleteAdapter);
+
+        mSearchText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                if(actionId == EditorInfo.IME_ACTION_SEARCH
+                        || actionId == EditorInfo.IME_ACTION_DONE
+                        || keyEvent.getAction() == KeyEvent.ACTION_DOWN
+                        || keyEvent.getAction() == KeyEvent.KEYCODE_ENTER){
+
+                    //execute our method for searching
+                    geoLocate();
+                }
+
+                return false;
+            }
+        });
+    }
+    private void geoLocate(){
+        Log.d(TAG, "geoLocate: geolocating");
+
+        String searchString = mSearchText.getText().toString();
+
+        Geocoder geocoder = new Geocoder(MapActivity.this);
+        List<Address> list = new ArrayList<>();
+        try{
+            list = geocoder.getFromLocationName(searchString, 1);
+        }catch (IOException e){
+            Log.e(TAG, "geoLocate: IOException: " + e.getMessage() );
+        }
+
+        if(list.size() > 0){
+            Address address = list.get(0);
+
+            Log.d(TAG, "geoLocate: found a location: " + address.toString());
+            //Toast.makeText(this, address.toString(), Toast.LENGTH_SHORT).show();
+            LatLng latLng = new LatLng(address.getLatitude(),address.getLongitude());
+            MarkerOptions options = new MarkerOptions()
+                    .position(latLng)
+                    .title(address.getAddressLine(0));
+            Log.d(TAG, "moveCamera: moving the camera to: lat: " + latLng.latitude + ", lng: " + latLng.longitude );
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, DEFAULT_ZOOM));
+            mMap.addMarker(options);
+        }
+    }
     private void getDeviceLocation(){
         Log.d(TAG, "getDeviceLocation: getting the devices current location");
 
@@ -138,8 +253,8 @@ void detect_bump(){
                             Log.d(TAG, "moveCamera: moving the camera to: lat: " + latLng.latitude + ", lng: " + latLng.longitude );
                             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, DEFAULT_ZOOM));
                             mMap.addMarker(options);
-                              longitude = latLng.longitude;
-                              latitude = latLng.latitude;
+                            longitude = latLng.longitude;
+                            latitude = latLng.latitude;
 
                             rootRef.child("Coordinates").orderByChild("x").startAt(29).endAt(32).addListenerForSingleValueEvent(new ValueEventListener() {
                                 @Override
@@ -151,7 +266,7 @@ void detect_bump(){
                                             double ydouble = issue.child("y").getValue(Double.class);
                                             double xdouble = issue.child("x").getValue(Double.class);
 
-                                            if(ydouble > 29 && ydouble < 32) {
+                                            //if(ydouble > 29 && ydouble < 32) {
                                                 // HashMap<String, Object> result = new HashMap<>();
                                                 //result.put("y", "345");
                                                 //DatabaseReference yref = issue.child("y").getRef();
@@ -170,8 +285,8 @@ void detect_bump(){
 
 
                                                 // usersRef = ref.child("Users").child(name);
-                                                return;
-                                            }
+                                              //  return;
+                                            //}
 
 
                                         }
@@ -179,9 +294,8 @@ void detect_bump(){
 
                                     }
 
-
-                                    double coorx =longitude ;
-                                    double coory =latitude ;
+                                    double coorx =latitude ;
+                                    double coory =longitude ;
                                     HashMap<String, Double> coordinates = new HashMap<String, Double>();
 
                                     coordinates.put("x", coorx);
@@ -192,7 +306,7 @@ void detect_bump(){
                                     // usersRef = ref.child("Users").child(name);
                                     demoRef.push().setValue(coordinates);
 
-                                    }
+                                }
 
 
 
@@ -213,6 +327,84 @@ void detect_bump(){
             Log.e(TAG, "getDeviceLocation: SecurityException: " + e.getMessage() );
         }
     }
+
+///////////////////////////////////
+
+
+
+    private void getCurrentLocation(){
+        Log.d(TAG, "getDeviceLocation: getting the devices current location");
+
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        try{
+            if(mLocationPermissionsGranted){
+
+                final Task location = mFusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if(task.isSuccessful()){
+                            Log.d(TAG, "onComplete: found location!");
+                            Location currentLocation = (Location) task.getResult();
+                            LatLng latLng = new LatLng(currentLocation.getLatitude(),currentLocation.getLongitude());
+
+                            Log.d(TAG, "moveCamera: moving the camera to: lat: " + latLng.latitude + ", lng: " + latLng.longitude );
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, DEFAULT_ZOOM));
+
+                            rootRef.child("Coordinates").orderByChild("x").addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(DataSnapshot dataSnapshot) {
+                                    if (dataSnapshot.exists()) {
+                                        // dataSnapshot is the "issue" node with all children with id 0
+                                        for (DataSnapshot issue : dataSnapshot.getChildren()) {
+                                            //String name = (String) issue.child("y").getValue();
+                                            double ydouble = issue.child("y").getValue(Double.class);
+                                            double xdouble = issue.child("x").getValue(Double.class);
+
+                                            LatLng bumpsLatLng = new LatLng(xdouble, ydouble);
+                                            MarkerOptions options = new MarkerOptions()
+                                                    .position(bumpsLatLng)
+                                                    .title("Bump!");
+                                            mMap.addMarker(options);
+
+                                        }
+                                    }
+                                }
+                                @Override
+                                public void onCancelled(DatabaseError databaseError) {
+
+                                }
+                            });
+
+
+                        }else{
+                            Log.d(TAG, "onComplete: current location is null");
+                            Toast.makeText(MapActivity.this, "unable to get current location", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        }catch (SecurityException e){
+            Log.e(TAG, "getDeviceLocation: SecurityException: " + e.getMessage() );
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     private void initMap(){
@@ -304,10 +496,17 @@ void detect_bump(){
         UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb"); // Standard
         // SerialPortService
         // ID
+
         mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);
+        Log.d(TAG, "ya raaaaaaaab");
+
         mmSocket.connect();
+        Log.d(TAG, "eshta3'al?");
+
         mmOutputStream = mmSocket.getOutputStream();
         mmInputStream = mmSocket.getInputStream();
+        Log.d(TAG, "haa");
+
         beginListenForData();
 
     }
@@ -322,9 +521,13 @@ void detect_bump(){
         readBuffer = new byte[1024];
         workerThread = new Thread(new Runnable() {
             public void run() {
+                Log.d(TAG, "ha b2aa");
+
                 while (!Thread.currentThread().isInterrupted() && !stopWorker) {
                     try {
                         int bytesAvailable = mmInputStream.available();
+                        //Log.d(TAG, "fe bytes?"+bytesAvailable);
+
                         if (bytesAvailable > 0) {
                             byte[] packetBytes = new byte[bytesAvailable];
                             mmInputStream.read(packetBytes);
@@ -341,6 +544,7 @@ void detect_bump(){
 
                                     handler.post(new Runnable() {
                                         public void run() {
+
                                             detect_bump();
 
                                         }
@@ -360,6 +564,8 @@ void detect_bump(){
 
         workerThread.start();
     }
+
+
 }
 
 
